9d4d3934ec7e25bdd851b440580dd88e6352d307
Your Name <you@example.com>
2021-04-13 12:24:21 +0700

    change

--- Home.js
+++ Home.js
@@ -6,24 +6,27 @@ import Menubottom from './MenuBottom'
 const DATA1 = [
   {
     id: 1,
-    photo: 'https://www.isic.nl/media/1094811/0716_isic_nlbanner_nl.jpg',
+    photo: 'https://cdn.msiapromos.com/wp-content/uploads/2014/08/Forever-21-21-Aug-2014-300x300.jpg',
+
   },
   {
     id: 2,
-    photo: 'https://www.isic.nl/media/1094811/0716_isic_nlbanner_nl.jpg',
+    photo: 'https://lh3.googleusercontent.com/R8BIUI6Quae8Y_2MB07A8hg3eG3DUh7n43P4kIGwdtZ6RzTulRZdJFHX_Wl7bBWK9xzmY_uFZC1sqGdcsSrhzMRdDw=w1000',
   },
   {
     id: 3,
-    photo: 'https://www.isic.nl/media/1094811/0716_isic_nlbanner_nl.jpg',
+    photo: 'https://lh3.googleusercontent.com/0gcUzImRxv7fOgeiQHrWeG8YK0uIEtsTh5dsZfojIzGLQBDTMZWweB4fuGCW_WmL4XtTkywv3oKU4rkiC3IBls9AwIug=w1000',
   },
   {
     id: 4,
+    
     photo: 'https://www.isic.nl/media/1094811/0716_isic_nlbanner_nl.jpg',
   }
 ];
 const DATA = [
   {
     id: 1,
+    
     photo: 'https://www.isic.nl/media/1094811/0716_isic_nlbanner_nl.jpg',
 
   },
@@ -72,7 +75,7 @@ const DATA = [
 export default function Home() {
 
   const renderItem = ({ item }) => (
-    <View style={{ marginTop: 20 }} >
+    <View style={{ marginTop: 12 }} >
       <Image
         style={styles.imgStyle}
         source={{ uri: item.photo, }}
@@ -108,6 +111,11 @@ export default function Home() {
       </View>
       
       <ScrollView>
+        <View style={{marginTop:12}}>
+          <Text style={{textAlign:'center',textDecorationLine:'underline'}}>
+            All Special Offers (12)
+          </Text>
+        </View>
         <View>
         <FlatList
         data={DATA}
@@ -154,6 +162,7 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
     flex: 1,
+    backgroundColor:'#ddd'
   },
   search: {
     width:50,
@@ -161,6 +170,7 @@ const styles = StyleSheet.create({
     flex: 0.95,
     justifyContent: 'center',
     alignItems: 'center'
+    
 
 
   },
